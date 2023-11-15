26606b6813de0c8c919c84b1276c2dd8e3117d50
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-11-15 19:10:51 +0100

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 export default function App() {
-  return <MantineProvider theme={theme}>
-    <div className="underline">App</div>
-  </MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </MantineProvider>
+  );
 }
