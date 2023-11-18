b969ed4e447e7b8269524c2933fc58e28e71126e
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-11-18 07:52:07 +0100

    added sign up page

--- src/pages/unauthenticated/auth/Register.tsx
+++ src/pages/unauthenticated/auth/Register.tsx
@@ -3,14 +3,14 @@ import { FcGoogle } from "react-icons/fc";
 import Logo from "../../../assets/svgs/logo.svg";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const navigate = useNavigate()
+const Register = () => {
+    const navigate = useNavigate()
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center px-6 lg:px-8">
       <img src={Logo} alt="" className="mt-10" />
       <div className="text-center">
-        <h1 className="font-bold text-2xl">Sign In</h1>
-        <div>Sign in to your account and start learning.</div>
+        <h1 className="font-bold text-2xl">Sign Up</h1>
+        <div>Sign up to your account and start learning.</div>
       </div>
       <div className="mt-10 bg-white border shadow px-[40px] lg:px-[100px] py-10 rounded-xl max-w-[650px] w-full">
         <TextInput
@@ -28,11 +28,17 @@ const Login = () => {
           placeholder="Enter your password"
           className="w-full"
         />
-        <div className="text-right text-primary hover:underline font-semibold cursor-pointer">
-          Forgot password?
-        </div>
+        <PasswordInput
+          required
+          mt={16}
+          size="md"
+          label="Confirm Password"
+          placeholder="Enter your password"
+          className="w-full"
+        />
+        
         <Button size="md" mt={24} className="bg-primary w-full">
-          Sign in
+          Sign up
         </Button>
         <div className="mt-10 flex gap-3 items-center">
           <div className="w-[45%] h-[1px] bg-secondary" />
@@ -51,11 +57,11 @@ const Login = () => {
       </div>
       <div className="my-10">
         Don’t have an account?{" "}
-        <span className="text-primary font-semibold cursor-pointer" onClick={() => navigate("/register")}>Create an account </span>
+        <span className="text-primary font-semibold cursor-pointer" onClick={() => navigate("/login")}>Sign in </span>
         now!
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
