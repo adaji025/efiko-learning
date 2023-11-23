c70539c15e0e42d664b05db1ea27a92b5c334666
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-11-23 08:11:02 +0100

    added faq page

--- src/components/data.ts
+++ src/components/data.ts
@@ -23,4 +23,39 @@ export const sessionData = [
       desc: `Algebra, the language of mathematics,To solve problems and explore new paths.`,
       duration: 3,
     },
-  ];
+];
+  
+export const learningData = [
+  {
+    title: `What is Efiko Learning? `,
+    text: `An eLearning platform is a software application that provides a virtual learning environment for students and learners to access educational resources and take online courses. It enables instructors to create and manage online courses, deliver learning materials, track student progress, and assess student performance.`,
+  },
+  {
+    title: `How to use Efiko Learning? `,
+    text: `We have already answered your most asked questions. Have a look at the
+    FAQs below.`,
+  },
+  {
+    title: `How Efiko Learning can benfit me? `,
+    text: `We have already answered your most asked questions. Have a look at the
+    FAQs below.`,
+  },
+];
+
+export const paymentData = [
+  {
+    title: `What payment method to use at Efiko Learning?  `,
+    text: `We have already answered your most asked questions. Have a look at the
+    FAQs below.`,
+  },
+  {
+    title: `How can I increase my earnings?`,
+    text: `We have already answered your most asked questions. Have a look at the
+    FAQs below.`,
+  },
+  {
+    title: `How much I can earn on Efiko Learning in one month?`,
+    text: `We have already answered your most asked questions. Have a look at the
+    FAQs below.`,
+  },
+];
